43471016bea24106b7783ac6e4f5dc480d141614
mikayla dorado <mikayla.dorado17@gmail.com>
2024-01-18 11:52:17 -0600

    completed display bikes

---
+++ Controllers/BikeController.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using BiancasBikes.Data;
+using Microsoft.EntityFrameworkCore;
+using BiancasBikes.Models;
+using BiancasBikes.Models.DTOs;
+
+namespace BiancasBikes.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BikeController : ControllerBase
+{
+    private BiancasBikesDbContext _dbContext;
+
+    public BikeController(BiancasBikesDbContext context)
+    {
+        _dbContext = context;
+    }
+
+    [HttpGet]
+    [Authorize]
+    public IActionResult Get()
+    {
+        return Ok(_dbContext
+            .Bikes.Include(b => b.Owner).ToList()
+            .Select(b => new BikeDTO
+            {
+                Id = b.Id,
+                Brand = b.Brand,
+                Color = b.Color,
+                BikeTypeId = b.BikeTypeId,
+                OwnerId = b.OwnerId,
+                Owner = new OwnerDTO
+                {
+                    Id = b.Owner.Id,
+                    Name = b.Owner.Name,
+                    Address = b.Owner.Address,
+                    Email = b.Owner.Email,
+                    Telephone = b.Owner.Telephone
+                }
+            })
+            .ToList());
+    }
+
+    [HttpGet("{id}")]
+    [Authorize]
+    public IActionResult GetById(int id)
+    {
+        Bike bike = _dbContext
+            .Bikes
+            .Include(b => b.Owner)
+            .Include(b => b.BikeType)
+            .Include(b => b.WorkOrders)
+            .SingleOrDefault(b => b.Id == id);
+
+        if (bike == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(bike);
+    }
+
+}
